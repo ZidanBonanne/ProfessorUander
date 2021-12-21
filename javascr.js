4cc2b0a290dcaee9a05e421e776b1d77bdf00277
Zidan Bonanne <95802975+ZidanBonanne@users.noreply.github.com>
2021-12-21 18:34:53 -0300

    Add files via upload

---
+++ javascr.js
@@ -0,0 +1,14 @@
+﻿const agendeAula = document.querySelectorAll('.agendarAula');
+const btn = document.querySelector('.agendarAula');
+const modal = document.querySelector('.modal');
+const btnClose = document.querySelector('.close-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+};
+btn.addEventListener('click', openModal);
+
+btnClose.addEventListener('click', closeModal);
