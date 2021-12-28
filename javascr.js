24ad1a0c16c26b53d7090ce59d17d54e1c49eb5c
Zidan Bonanne <95802975+ZidanBonanne@users.noreply.github.com>
2021-12-27 22:59:52 -0300

    Add files via upload

--- javascr.js
+++ javascr.js
@@ -1,7 +1,11 @@
-﻿const agendeAula = document.querySelectorAll('.agendarAula');
+﻿// const agendeAula = document.querySelectorAll('.agendarAula');
 const btn = document.querySelector('.agendarAula');
 const modal = document.querySelector('.modal');
 const btnClose = document.querySelector('.close-modal');
+const btnDuvida1 = document.querySelector('.duvida1');
+const respostaDuvida1 = document.querySelector('.respostaDuvida1');
+const btnDuvida2 = document.querySelector('.duvida2');
+const respostaDuvida2 = document.querySelector('.respostaDuvida2');
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -12,3 +16,16 @@ const closeModal = function () {
 btn.addEventListener('click', openModal);
 
 btnClose.addEventListener('click', closeModal);
+
+const openDuvida1 = function () {
+  respostaDuvida1.classList.remove('hidden');
+  console.log('funcinou');
+};
+
+btnDuvida1.addEventListener('click', openDuvida1);
+const openDuvida2 = function () {
+  respostaDuvida2.classList.remove('hidden');
+  console.log('funcinou');
+};
+
+btnDuvida2.addEventListener('click', openDuvida2);
