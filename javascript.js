0998981fbadb5b233290d59b046b4341e630bd51
Zidan Bonanne <95802975+ZidanBonanne@users.noreply.github.com>
2022-02-26 20:24:14 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,36 @@
+﻿const respostaDuvida1 = document.querySelector(".respostaDuvida1");
+const respostaDuvida2 = document.querySelector(".respostaDuvida2");
+const respostaDuvida3 = document.querySelector(".respostaDuvida3");
+
+// const btnDuvida2 = document.querySelector(".duvida2");
+
+// const btnDuvida1 = document.querySelector(".duvida1");
+
+let contadorDuvida1 = 0;
+function openDuvida1() {
+  contadorDuvida1 += 1;
+  if (contadorDuvida1 % 2 === 1) {
+    respostaDuvida1.classList.remove("hidden");
+  } else {
+    respostaDuvida1.classList.add("hidden");
+  }
+}
+let contadorDuvida2 = 0;
+function openDuvida2() {
+  contadorDuvida2 += 1;
+  if (contadorDuvida2 % 2 === 1) {
+    respostaDuvida2.classList.remove("hidden");
+  } else {
+    respostaDuvida2.classList.add("hidden");
+  }
+}
+
+let contadorDuvida3 = 0;
+function openDuvida3() {
+  contadorDuvida3 += 1;
+  if (contadorDuvida3 % 3 === 1) {
+    respostaDuvida3.classList.remove("hidden");
+  } else {
+    respostaDuvida3.classList.add("hidden");
+  }
+}
